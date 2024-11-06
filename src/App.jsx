b25cae8f6af8ab5f3b79f7b0e50ac3489ab81a4c
Goutam khanna <123886841+Dark1Arrow@users.noreply.github.com>
2024-11-06 22:13:23 +0530

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -31,14 +31,16 @@ function App() {
       <div className='bg-[#111111] text-white' style={{ font: "popins" }}>
 
 
-        <HashRouter>
+        {/* <HashRouter>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/Project" element={<Project />} />
             <Route path="/About" element={<About />} />
             <Route path="/Contact" element={<Contact />} />
           </Routes>
-        </HashRouter>
+        </HashRouter> */}
+
+        <div className='bg-black text-white'>hello ther is page </div>
 
       </div>
     </>
