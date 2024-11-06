c003dfa3525f97a06880d4252b3cdf0277df696e
Goutam khanna <123886841+Dark1Arrow@users.noreply.github.com>
2024-11-06 21:01:32 +0530

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { HashRouter, Routes, Route } from 'react-router-dom'
-import Home from './Components/Home'
-import Project from './Components/Project'
-import About from './Components/About'
-import Contact from './Components/Contact'
+// import { HashRouter, Routes, Route } from 'react-router-dom'
+// import Home from './Components/Home'
+// import Project from './Components/Project'
+// import About from './Components/About'
+// import Contact from './Components/Contact'
 function App() {
 
   // const Router = createBrowserRouter([
@@ -40,7 +40,7 @@ function App() {
           </Routes>
         </HashRouter>
 */}
-        <Home />
+{/*         <Home /> */}
         <div> hello see teseting</div>
 
       </div>
